73ec937b2123c2f4595a7a218502ed89bfb0e28f
JasperRutte <jasperrutte@gmail.com>
2023-10-14 19:28:03 +0200

    trying to get everything into the same file

--- jasperRutteDev/src/App.vue
+++ jasperRutteDev/src/App.vue
@@ -1,6 +1,3 @@
-<script setup>
-</script>
-
 <template>
     <header>
         <h3>Hi, my name is</h3>
@@ -8,13 +5,47 @@
         <h2>And im a software developer</h2>
     </header>
     <main>
-        <router-link to="About">About me </router-link> |
-        <router-link to="Experience">Experiences </router-link> |
-        <router-link to="Links">Links</router-link>
+        <router-link @click="aboutExperienceLinks('About')" to="About">About me </router-link> |
+        <router-link @click="aboutExperienceLinks('Experience')" to="Experience">Experiences </router-link> |
+        <router-link @click="aboutExperienceLinks('Links')"  to="Links">Links</router-link>
 
         <router-view></router-view>
     </main>
 </template>
+
+<script setup>
+    export default {
+        data() {
+            return{
+                about: false,
+                experience: false,
+                links: false
+            }
+        },
+        mounted() {
+        },
+        methods: {
+            aboutExperienceLinks(view){
+                if (view === "About"){
+                    this.about = true
+                    this.experience = false
+                    this.links = false
+                } else if (view === "Experience"){
+                    this.about = false
+                    this.experience = true
+                    this.links = false
+                } else if (view === "Links"){
+                    this.about = false
+                    this.experience = false
+                    this.links = true
+                } else {
+                    console.log("error")
+                }
+            }
+        }
+    }
+</script>
+
 
 <style scoped>
 
